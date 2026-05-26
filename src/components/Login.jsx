7e81d38ab695c9cb607c98bc893b865e90344d14
Bhupesh <malibhupesh58@gmail.com>
2026-05-26 11:37:22 +0530

    ser Error message to useState

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -32,6 +32,7 @@ const Login = () => {
                 .catch((error) => {
                     const errorCode = error.code;
                     const errorMessage = error.message;
+                    setValidationError(errorCode + " - " + errorMessage)
                     console.log(errorCode + " - " + errorMessage);
 
                 });
@@ -45,6 +46,7 @@ const Login = () => {
                 .catch((error) => {
                     const errorCode = error.code;
                     const errorMessage = error.message;
+                    setValidationError(errorCode + " - " + errorMessage)
                     console.log("@@@@Error", errorCode + " - " + errorMessage);
 
                 });
